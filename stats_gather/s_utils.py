0bc03c4d021c6e7adf278a1657358bfa62ea9db8
TheRealGabHas <contact@gabhas.fr>
2024-07-17 12:14:50 +0200

    Added docstring for the get_level_from_xp function

--- stats_gather/s_utils.py
+++ stats_gather/s_utils.py
@@ -14,6 +14,12 @@ def order_dict(_dict: dict, key_order: list):
 
 
 def get_level_from_xp(xp: str, skill_table: list[int]) -> int:
+    """
+    Return the player level given an EXP amount and a skill leveling table
+    :param xp: The amount of experience (a string such as "12,548,397")
+    :param skill_table: A list of the required amount of experience for each level (such as [10, 50, 175, 500, ...]
+    :return: A integer corresponding to the player level
+    """
     skill_xp = int(xp.replace(",", ""))
 
     for i, lvl in enumerate(skill_table):
