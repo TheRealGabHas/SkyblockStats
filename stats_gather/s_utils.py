25f04fc77a0fd6a832d9bd80d4fa75d6d96614e6
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2024-07-23 15:47:28 +0200

    fix: Typo

--- stats_gather/s_utils.py
+++ stats_gather/s_utils.py
@@ -18,7 +18,7 @@ def get_level_from_xp(xp: str, skill_table: list[int]) -> int:
     Return the player level given an EXP amount and a skill leveling table
     :param xp: The amount of experience (a string such as "12,548,397")
     :param skill_table: A list of the required amount of experience for each level (such as [10, 50, 175, 500, ...])
-    :return: A integer corresponding to the player level
+    :return: An integer corresponding to the player level
     """
     skill_xp = int(xp.replace(",", ""))
 
