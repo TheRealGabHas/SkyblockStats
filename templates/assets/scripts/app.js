08d1baebe01ccc80d921792c018ed632b6605c78
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2024-07-30 15:48:06 +0200

    fix(chocolate-factory): Added color to the trophy fish rarity

--- templates/assets/scripts/app.js
+++ templates/assets/scripts/app.js
@@ -2,13 +2,27 @@
     Javascript file for the statistics page (/p/{name}/ and /p/{name}/{profile})
  */
 
+
+// Handle the profile selector update
 const profileSelector = document.getElementById('profile-selector')
 const playerName = document.getElementById('main-skin-img').alt.toString()
 
 profileSelector.addEventListener('change', () => {
     const currentPage = this.window.location
     console.log(currentPage)
-    const nextProfile = profileSelector.value.split(" ")[0].replace(" ", "")
+    const nextProfile = profileSelector.value.split(" ")[0].replace(' ', '')
 
     this.window.location = '/p/' + playerName + '/' + nextProfile
+})
+
+
+// Set the color for the Trophy fish rarities
+const rarityColor = {
+    "Bronze": "var(--§4)",
+    "Silver": "var(--§7)",
+    "Gold": "#d9a334",
+    "Diamond": "var(--§b)",
+}
+document.querySelectorAll('.trophy-count p strong').forEach(element => {
+    element.style.color = rarityColor[element.innerHTML.toString()]
 })
